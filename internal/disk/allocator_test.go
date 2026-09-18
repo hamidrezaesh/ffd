9fe746db48b049776da474428f563a4a4f64be13
Hamidreza <161152077+hamidrezaesh@users.noreply.github.com>
2026-09-18 13:44:11 +0330

    test: add allocator disk

---
+++ internal/disk/allocator_test.go
@@ -0,0 +1,24 @@
+package disk
+
+import (
+	"testing"
+	"os"
+)
+
+func TestAllocate(t *testing.T){
+	f := FileInfo {
+		Filename: "example.test",
+		Path: "example.test",
+		TotalSize: int64(1200000),
+	}
+
+	file, err := Allocate(f)
+	if err != nil{
+		t.Fatalf("Error: %v", err)
+	}
+
+	i, _ := os.Stat("example.test")
+	size := i.Size()
+
+	t.Logf("\nFilename: %v\nPath: %v\nExpectedSize: %v\nFile: %v\nFinalSize: %v", f.Filename, f.Path, f.TotalSize, file, size)
+}
