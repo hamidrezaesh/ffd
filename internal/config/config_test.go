8e6b27be5593a5e0e56cf30a099c0a228b7464c2
Hamidreza <161152077+hamidrezaesh@users.noreply.github.com>
2026-09-22 18:45:03 +0330

    test(config): add test config

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,1 +1,39 @@
 package config
+
+import (
+	"testing"
+)
+
+func TestGetConfig(t *testing.T) {
+	config, err := GetConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Config: %+v", config)
+}
+
+func TestSetConfig(t *testing.T) {
+	config, err := GetConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Before SetConfig: %+v", config)
+
+	config.Headers = append(config.Headers, Header{
+		Key:   "X-Test",
+		Value: "hello",
+	})
+
+	if err := SetConfig(config); err != nil {
+		t.Fatal(err)
+	}
+
+	updated, err := GetConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("After SetConfig: %+v", updated)
+}
